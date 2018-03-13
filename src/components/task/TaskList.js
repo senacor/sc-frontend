592f2e0f6b261680cb7d49ad8ce52df3c23c8aa0
Ediba Zugor <Ediba.Zugor@senacor.com>
2018-03-13 16:40:42 +0100

    Added Link to Task

--- src/components/task/TaskList.js
+++ src/components/task/TaskList.js
@@ -7,23 +7,52 @@ import withLoading from '../hoc/Loading';
 import Deadline from './Deadline';
 import { Link } from 'react-router-dom';
 
-export const TaskList = props => (
-  <div>
-    {props.tasks.map(task => (
-      <Card key={task.id} className="task">
-        <CardContent className={task.id}>
-          <Link to={`/prs/${task.id}`} style={{ textDecoration: 'none' }}>
-            <Typography variant="headline" component="h2">
-              {task.title}
-            </Typography>
-            <Typography component="p">{task.description}</Typography>
-            <Deadline deadline={task.deadline} />
-          </Link>
-        </CardContent>
-      </Card>
-    ))}
-  </div>
-);
+export class TaskList extends React.Component {
+  handleClick = task => {
+    task.status = 'DONE';
+    let tempArray = [];
+
+    for (let i = 0; i < this.state.tasks.length; i++) {
+      tempArray[i] = this.state.tasks[i];
+    }
+    this.setState({
+      tasks: tempArray
+    });
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {tasks: props.tasks};
+  }
+
+  render() {
+    return (
+      <div>
+        {props.tasks.map(task => (
+          <Card key={task.id} className="task">
+            <CardContent className={task.id}>
+              <Link to={`/prs/${task.id}`} style={{textDecoration: 'none'}}>
+                <Typography variant="headline" component="h2">
+                  {task.title}
+                </Typography>
+                <Typography component="p">{task.description}</Typography>
+                <Deadline deadline={task.deadline}/>
+                <button
+                  onClick={() => {
+                    this.handleClick(task);
+                  }}
+                >
+                  {' '}
+                  {task.status}
+                </button>
+              </Link>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default connect(
   state => ({
