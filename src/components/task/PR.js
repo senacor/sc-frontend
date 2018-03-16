b45e688b9c4ceae7a25d3ed5a9a2a873fc62e791
Ediba Zugor <Ediba.Zugor@senacor.com>
2018-03-16 13:06:20 +0100

    PR-78: list of all PRs added

--- src/components/task/PR.js
+++ src/components/task/PR.js
@@ -1,13 +1,46 @@
-import React, { Component } from 'react';
 
-class PR extends Component {
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+import Card, {CardContent } from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+import withLoading from '../hoc/Loading';
+
+export class PR extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      prs: props.prs
+    };
+  }
+
   render() {
+    const { prs } = this.props;
     return (
       <div>
-        <h2>HELLO</h2>
-        <p>Ne mozes da sjedis sa nama!</p>
+        {prs.map(pr => (
+          <Card key={pr.id} id="pr">
+            <CardContent>
+                <Typography variant="headline" component="h2">
+                  {pr.occasion}
+                </Typography>
+                <Typography component="p">{pr.employee}</Typography>
+              <Typography component="p">{pr.supervisor}</Typography>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     );
   }
 }
-export default PR;
+
+
+export default connect(
+  state => ({
+    prs: state.prs.PRList,
+    isLoadingPRs: state.prs.isLoadingPRs,
+  }),
+  {
+    fetchPrs: actions.fetchPrs,
+  }
+)(withLoading(props => props.fetchPrs())(PR));
