47639b120d753156887e4af3b3f611308c04bf98
Ediba Zugor <Ediba.Zugor@senacor.com>
2018-04-05 08:49:12 +0200

    PR-38: The returning state of the PRs has been changed

--- src/reducers/prs.js
+++ src/reducers/prs.js
@@ -3,9 +3,10 @@ import { combineReducers } from 'redux';
 const prsList = (state = [], action) => {
   switch (action.type) {
     case 'FETCH_PRS_RESPONSE':
-      return [...state, { prs: action.prs }];
+      return action.prs;
     case 'ADD_PR_RESPONSE':
-      return [...state, { pr: action.pr }];
+      return [...state, action.pr];
+
     default:
       return state;
   }
