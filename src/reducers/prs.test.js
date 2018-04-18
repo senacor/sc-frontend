0012e8b0cb87fbffc06059fac44a397146a8f0c9
Ediba Zugor <Ediba.Zugor@senacor.com>
2018-04-18 16:13:24 +0200

    PR-171 tests for the reducer added

--- src/reducers/prs.test.js
+++ src/reducers/prs.test.js
@@ -27,4 +27,53 @@ describe('prs reducer', () => {
 
     expect(stateAfter).toEqual({ prsList: ['pr2'] });
   });
+
+  it('should test the reducer for ADD_SUPERVISOR', () => {
+    const stateBefore = {
+      prsList: []
+    };
+
+    let pr = {
+      id: 1,
+      employee: 'emp',
+      supervisor: 'fukara',
+      occasion: 'ON_DEMAND',
+      status: 'PREPARATION',
+      deadline: '2018-03-14',
+      _links: {
+        self: {
+          href: 'http://localhost:8010/api/v1/prs/22'
+        }
+      }
+    };
+
+    const actionAddingPr = {
+      type: 'ADD_PR_RESPONSE',
+      pr: pr
+    };
+    const actionAddingDelegatedSupervisor = {
+      type: 'ADD_SUPERVISOR',
+      supervisor: 'dummy',
+      prId: 1
+    };
+
+    const stateAfter = prs(
+      prs(stateBefore, actionAddingPr),
+      actionAddingDelegatedSupervisor
+    );
+
+    expect(stateAfter).toEqual({
+      prsList: [
+        {
+          deadline: '2018-03-14',
+          delegateSupervisor: 'dummy',
+          employee: 'emp',
+          id: 1,
+          occasion: 'ON_DEMAND',
+          status: 'PREPARATION',
+          supervisor: 'fukara'
+        }
+      ]
+    });
+  });
 });
