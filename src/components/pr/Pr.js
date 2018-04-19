3599d2a80d157809e63a9ac9b4839e55914be0b2
Ediba Zugor <Ediba.Zugor@senacor.com>
2018-04-19 18:06:10 +0200

    PR-37: two new components added, whole desktop screen is used for showing the status, salary and employment

--- src/components/pr/Pr.js
+++ src/components/pr/Pr.js
@@ -7,6 +7,9 @@ import { connect } from 'react-redux';
 import withLoading from '../hoc/Loading';
 import * as actions from '../../actions/index';
 import PrState from './PrState';
+import PrSalary from './PrSalary';
+import PrEmployment from './PrEmployment';
+import Hidden from 'material-ui/Hidden';
 
 const styles = theme => ({
   avatar: {
@@ -25,6 +28,13 @@ const styles = theme => ({
     marginTop: '20px',
     fontSize: '15px'
   },
+  typographyTabs: {
+    color: '#FFF',
+    marginTop: '20px',
+    fontSize: '15px',
+    width: '33.3%',
+    marginLeft: '15%'
+  },
 
   detailPanel: {
     width: '100%',
@@ -34,6 +44,24 @@ const styles = theme => ({
 
   tabsColor: {
     color: theme.palette.contrastText
+  },
+  label: {
+    width: '33.3%'
+  },
+  containerTabs: {
+    display: 'flex',
+
+    width: '100%',
+    backgroundColor: theme.palette.primary['400'],
+    paddingBottom: '10px'
+  },
+  prTabs: {
+    width: '33.3%'
+  },
+  prDetailsWholeView: {
+    display: 'flex',
+
+    width: '100%'
   }
 });
 
@@ -111,30 +139,53 @@ class Pr extends React.Component {
               )}
             </Typography>
           </div>
-          <Tabs
-            value={this.state.value}
-            onChange={this.handleChange}
-            fullWidth
-            indicatorColor="#FFA07A"
-            className={classes.tabsColor}
-          >
-            <Tab label="STATUS" />
-            <Tab label="ANSTELLUNG" />
-            <Tab label="GEHALT" />
-          </Tabs>
+          <Hidden smUp>
+            <Tabs
+              value={this.state.value}
+              onChange={this.handleChange}
+              indicatorColor="#FFA07A"
+              className={classes.tabsColor}
+            >
+              <Tab className={classes.label} label="STATUS" />
+              <Tab className={classes.label} label="ANSTELLUNG" />
+              <Tab className={classes.label} label="GEHALT" />
+            </Tabs>
+          </Hidden>
         </div>
+        <Hidden smUp>
+          {value === 0 && <PrState expanded={this.state.expanded} />}
+          {value === 1 && (
+            <TabContainer>
+              Hier steht das Anstellungsverhältnis des Mitarbeiters
+            </TabContainer>
+          )}
+          {value === 2 && (
+            <TabContainer>
+              Hier steht die Gehaltshistorie des Mitarbeiters
+            </TabContainer>
+          )}
+        </Hidden>
+        <Hidden smDown>
+          <div className={classes.containerTabs}>
+            <Typography className={classes.typographyTabs}>STATUS</Typography>
+            <Typography className={classes.typographyTabs}>
+              ANSTELLUNG
+            </Typography>
+            <Typography className={classes.typographyTabs}>GEHALT</Typography>
+          </div>
+          <div className={classes.prDetailsWholeView}>
+            <div className={classes.prTabs}>
+              <PrState expanded={this.state.expanded} />
+            </div>
+            <div className={classes.prTabs}>
+              <PrSalary />
+            </div>
 
-        {value === 0 && <PrState expanded={this.state.expanded} />}
-        {value === 1 && (
-          <TabContainer>
-            Hier steht das Anstellungsverhältnis des Mitarbeiters
-          </TabContainer>
-        )}
-        {value === 2 && (
-          <TabContainer>
-            Hier steht die Gehaltshistorie des Mitarbeiters
-          </TabContainer>
-        )}
+            <div className={classes.prTabs}>
+              <PrEmployment />
+            </div>
+          </div>
+        </Hidden>
       </div>
     );
   }
---
+++ src/components/pr/PrSalary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Typography from 'material-ui/Typography';
+
+class PrSalary extends React.Component {
+  render() {
+    return (
+      <div>
+        <Typography style={{ textAlign: 'center' }}>
+          Hier steht die Gehaltshistorie des Mitarbeiters
+        </Typography>
+      </div>
+    );
+  }
+}
+export default PrSalary;
