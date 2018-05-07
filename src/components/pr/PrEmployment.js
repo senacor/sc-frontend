892f16c128fe38c0753596c96531e0116ea36f01
Ediba Zugor <Ediba.Zugor@senacor.com>
2018-05-07 16:07:14 +0200

    PR-195: Unit test + snapshot, jobGrading added, if the elements don't exist, don't show

--- src/components/pr/PrEmployment.js
+++ src/components/pr/PrEmployment.js
@@ -29,135 +29,189 @@ class PrEmployment extends React.Component {
     console.log(prById);
     return (
       <Paper>
-        <List>
-          <ListItem>
-            <ListItemText>
-              <div>
-                <Typography className={classes.title}>
-                  Ende der Probezeit:
-                </Typography>
-                <Typography className={classes.listItems}>
-                  {`${moment(
-                    prById.employee.employment.endOfProbationPeriod
-                  ).format('DD.MM.YY')}`}
-                </Typography>
-              </div>
-            </ListItemText>
-          </ListItem>
-        </List>
+        {prById.employee.employment.endOfProbationPeriod ? (
+          <List>
+            <ListItem>
+              <ListItemText>
+                <div>
+                  <Typography className={classes.title}>
+                    Ende der Probezeit:
+                  </Typography>
+                  <Typography className={classes.listItems}>
+                    {`${moment(
+                      prById.employee.employment.endOfProbationPeriod
+                    ).format('DD.MM.YY')}`}
+                  </Typography>
+                </div>
+              </ListItemText>
+            </ListItem>
+          </List>
+        ) : (
+          ''
+        )}
         <Divider />
-        <List>
-          <ListItem>
-            <ListItemText>
-              <div>
-                <Typography className={classes.title}>Mutterschutz:</Typography>
 
-                {prById.employee.employment.leaves.maternityLeave.map(
-                  maternityLeave => {
+        {prById.employee.employment.leaves.maternityLeave ? (
+          <List>
+            <ListItem>
+              <ListItemText>
+                <div>
+                  <Typography className={classes.title}>
+                    Mutterschutz:
+                  </Typography>
+
+                  {prById.employee.employment.leaves.maternityLeave.map(
+                    maternityLeave => {
+                      return (
+                        <Typography
+                          className={classes.listItems}
+                          key={prById.employee.employment.leaves.maternityLeave.indexOf(
+                            maternityLeave
+                          )}
+                        >
+                          {`${moment(maternityLeave.from).format(
+                            'DD.MM.YY'
+                          )} bis ${moment(maternityLeave.to).format(
+                            'DD.MM.YY'
+                          )}`}
+                        </Typography>
+                      );
+                    }
+                  )}
+                </div>
+              </ListItemText>
+            </ListItem>
+          </List>
+        ) : (
+          ''
+        )}
+        <Divider />
+        {prById.employee.employment.leaves.parentalLeave ? (
+          <List>
+            <ListItem>
+              <ListItemText>
+                <div>
+                  <Typography className={classes.title}>Elternzeit:</Typography>
+
+                  {prById.employee.employment.leaves.parentalLeave.map(
+                    parentalLeave => {
+                      return (
+                        <Typography
+                          className={classes.listItems}
+                          key={prById.employee.employment.leaves.parentalLeave.indexOf(
+                            parentalLeave
+                          )}
+                        >
+                          {` ${moment(parentalLeave.from).format(
+                            'DD.MM.YY'
+                          )} bis ${moment(parentalLeave.to).format(
+                            'DD.MM.YY'
+                          )}`}
+                        </Typography>
+                      );
+                    }
+                  )}
+                </div>
+              </ListItemText>
+            </ListItem>
+          </List>
+        ) : (
+          ''
+        )}
+        <Divider />
+        {prById.employee.employment.leaves.sabbatical ? (
+          <List>
+            <ListItem>
+              <ListItemText>
+                <div>
+                  <Typography className={classes.title}>
+                    Forschungsurlaub:
+                  </Typography>
+                  {prById.employee.employment.leaves.sabbatical.map(
+                    sabbatical => {
+                      return (
+                        <Typography
+                          className={classes.listItems}
+                          key={prById.employee.employment.leaves.sabbatical.indexOf(
+                            sabbatical
+                          )}
+                        >
+                          {` ${moment(sabbatical.from).format(
+                            'DD.MM.YY'
+                          )} bis ${moment(sabbatical.to).format('DD.MM.YY')}`}
+                        </Typography>
+                      );
+                    }
+                  )}
+                </div>
+              </ListItemText>
+            </ListItem>
+          </List>
+        ) : (
+          ''
+        )}
+        <Divider />
+        {prById.employee.employment.leaves.unpaidLeave ? (
+          <List>
+            <ListItem>
+              <ListItemText>
+                <div>
+                  <Typography className={classes.title}>
+                    Unbezahlter Urlaub:
+                  </Typography>
+                  {prById.employee.employment.leaves.unpaidLeave.map(
+                    unpaidLeave => {
+                      return (
+                        <Typography
+                          className={classes.listItems}
+                          key={prById.employee.employment.leaves.unpaidLeave.indexOf(
+                            unpaidLeave
+                          )}
+                        >
+                          {` ${moment(unpaidLeave.from).format(
+                            'DD.MM.YY'
+                          )} bis ${moment(unpaidLeave.to).format('DD.MM.YY')}`}
+                        </Typography>
+                      );
+                    }
+                  )}
+                </div>
+              </ListItemText>
+            </ListItem>
+          </List>
+        ) : (
+          ''
+        )}
+        <Divider />
+        {prById.employee.employment.jobGradings ? (
+          <List>
+            <ListItem>
+              <ListItemText>
+                <div>
+                  <Typography className={classes.title}>
+                    Berufseinstufungen:
+                  </Typography>
+                  {prById.employee.employment.jobGradings.map(jobGradings => {
                     return (
                       <Typography
                         className={classes.listItems}
-                        key={prById.employee.employment.leaves.maternityLeave.indexOf(
-                          maternityLeave
+                        key={prById.employee.employment.jobGradings.indexOf(
+                          jobGradings
                         )}
                       >
-                        {`${moment(maternityLeave.from).format(
-                          'DD.MM.YY'
-                        )} bis ${moment(maternityLeave.to).format('DD.MM.YY')}`}
+                        {`Stufe ${jobGradings.salaryLevel} seit: ${moment(
+                          jobGradings.validFrom
+                        ).format('DD.MM.YY')}`}
                       </Typography>
                     );
-                  }
-                )}
-              </div>
-            </ListItemText>
-          </ListItem>
-        </List>
-        <Divider />
-
-        <List>
-          <ListItem>
-            <ListItemText>
-              <div>
-                <Typography className={classes.title}>Elternzeit:</Typography>
-
-                {prById.employee.employment.leaves.parentalLeave.map(
-                  parentalLeave => {
-                    return (
-                      <Typography
-                        className={classes.listItems}
-                        key={prById.employee.employment.leaves.parentalLeave.indexOf(
-                          parentalLeave
-                        )}
-                      >
-                        {` ${moment(parentalLeave.from).format(
-                          'DD.MM.YY'
-                        )} bis ${moment(parentalLeave.to).format('DD.MM.YY')}`}
-                      </Typography>
-                    );
-                  }
-                )}
-              </div>
-            </ListItemText>
-          </ListItem>
-        </List>
-        <Divider />
-
-        <List>
-          <ListItem>
-            <ListItemText>
-              <div>
-                <Typography className={classes.title}>
-                  Forschungsurlaub:
-                </Typography>
-                {prById.employee.employment.leaves.sabbatical.map(
-                  sabbatical => {
-                    return (
-                      <Typography
-                        className={classes.listItems}
-                        key={prById.employee.employment.leaves.sabbatical.indexOf(
-                          sabbatical
-                        )}
-                      >
-                        {` ${moment(sabbatical.from).format(
-                          'DD.MM.YY'
-                        )} bis ${moment(sabbatical.to).format('DD.MM.YY')}`}
-                      </Typography>
-                    );
-                  }
-                )}
-              </div>
-            </ListItemText>
-          </ListItem>
-        </List>
-        <Divider />
-
-        <List>
-          <ListItem>
-            <ListItemText>
-              <div>
-                <Typography className={classes.title}>
-                  Unbezahlter Urlaub:
-                </Typography>
-                {prById.employee.employment.leaves.unpaidLeave.map(
-                  unpaidLeave => {
-                    return (
-                      <Typography
-                        className={classes.listItems}
-                        key={prById.employee.employment.leaves.unpaidLeave.indexOf(
-                          unpaidLeave
-                        )}
-                      >
-                        {` ${moment(unpaidLeave.from).format(
-                          'DD.MM.YY'
-                        )} bis ${moment(unpaidLeave.to).format('DD.MM.YY')}`}
-                      </Typography>
-                    );
-                  }
-                )}
-              </div>
-            </ListItemText>
-          </ListItem>
-        </List>
+                  })}
+                </div>
+              </ListItemText>
+            </ListItem>
+          </List>
+        ) : (
+          ''
+        )}
       </Paper>
     );
   }
