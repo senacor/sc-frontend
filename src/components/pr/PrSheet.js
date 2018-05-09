34897ee9ffffc486cf6b7c270c35235858ef06aa
Ediba Zugor <Ediba.Zugor@senacor.com>
2018-05-09 09:59:14 +0200

    PR-197: Adding the kommentar placeholder, chosing a grading value between 1 and 5, also a description for the PRSheet

--- src/components/pr/PrSheet.js
+++ src/components/pr/PrSheet.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Divider from 'material-ui/Divider';
 import List, { ListItem, ListItemText } from 'material-ui/List';
+import TextField from 'material-ui/TextField';
+import MenuItem from 'material-ui/Menu/MenuItem';
+import classNames from 'classnames';
+import Collapse from 'material-ui/transitions/Collapse';
+import Icon from 'material-ui/Icon';
 import { withStyles } from 'material-ui/styles/index';
 
-const styles = () => ({
+const styles = theme => ({
   containerVertical: {
     flex: 1,
     flexDirection: 'column',
@@ -14,18 +19,113 @@ const styles = () => ({
   nested: {
     paddingLeft: '30px'
   },
+  nestedText: {
+    paddingLeft: '30px',
+    paddingRight: '30px'
+  },
   rightAlignText: {
     textAlign: 'right'
+  },
+  bootstrapRoot: {
+    padding: 0,
+    'label + &': {
+      marginTop: theme.spacing.unit * 3
+    }
+  },
+  bootstrapInput: {
+    borderRadius: 4,
+    backgroundColor: theme.palette.common.white,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 12px',
+    width: 'calc(100% - 24px)',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(','),
+    '&:focus': {
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)'
+    }
   }
 });
+
+const gradingValues = [
+  {
+    value: '1',
+    label: '1'
+  },
+  {
+    value: '2',
+    label: '2'
+  },
+  {
+    value: '3',
+    label: '3'
+  },
+  {
+    value: '4',
+    label: '4'
+  },
+  {
+    value: '5',
+    label: '5'
+  }
+];
 
 class PrSheet extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      prById: this.props.prById
+      prById: this.props.prById,
+      arbeitsweise: '1',
+      arbeitsergebnisse: '1',
+      problemanalyse: '1',
+      openProblemanalyse: false,
+      openArbeitsergebnisse: false,
+      openArbeitsweise: false
     };
   }
+  handleChange = prop => event => {
+    this.setState({ [prop]: event.target.value });
+  };
+
+  handleClick = value => {
+    switch (value) {
+      case 'openArbeitsweise':
+        this.setState({
+          openArbeitsweise: !this.state.openArbeitsweise,
+          openProblemanalyse: false,
+          openArbeitsergebnisse: false
+        });
+        break;
+      case 'openArbeitsergebnisse':
+        this.setState({
+          openArbeitsergebnisse: !this.state.openArbeitsergebnisse,
+          openProblemanalyse: false,
+          openArbeitsweise: false
+        });
+        break;
+      case 'openProblemanalyse':
+        this.setState({
+          openProblemanalyse: !this.state.openProblemanalyse,
+          openArbeitsergebnisse: false,
+          openArbeitsweise: false
+        });
+        break;
+      default:
+        return;
+    }
+  };
 
   render() {
     const { classes } = this.props;
@@ -38,27 +138,208 @@ class PrSheet extends React.Component {
               <ListItemText primary="Leistungen im Projekt" />
             </ListItem>
             <List disablePadding>
-              <ListItem button className={classes.nested}>
+              <ListItem
+                button
+                className={classes.nested}
+                onClick={() => this.handleClick('openProblemanalyse')}
+              >
                 <ListItemText secondary="Problemanalyse und -lösung" />
-                <ListItemText
-                  className={classes.rightAlignText}
-                  primary="4 / 5"
-                />
+                {this.state.openProblemanalyse ? (
+                  <Icon>expand_less</Icon>
+                ) : (
+                  <Icon>expand_more</Icon>
+                )}
+
+                <TextField
+                  select
+                  className={classNames(classes.margin, classes.textField)}
+                  value={this.state.problemanalyse}
+                  onChange={this.handleChange('problemanalyse')}
+                >
+                  {gradingValues.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
               </ListItem>
-              <ListItem button className={classes.nested}>
+              <Collapse
+                in={this.state.openProblemanalyse}
+                timeout="auto"
+                unmountOnExit
+              >
+                <List
+                  component="div"
+                  disablePadding
+                  className={classes.nestedText}
+                >
+                  <ListItem>
+                    <TextField
+                      id="multiline-flexible"
+                      label="Kommentar"
+                      multiline
+                      fullWidth
+                      rowsMax="4"
+                      value={this.state.multiline}
+                      onChange={this.handleChange('multiline')}
+                      InputProps={{
+                        disableUnderline: true,
+                        classes: {
+                          root: classes.bootstrapRoot,
+                          input: classes.bootstrapInput
+                        }
+                      }}
+                      InputLabelProps={{
+                        shrink: true,
+                        className: classes.bootstrapFormLabel
+                      }}
+                    />
+                  </ListItem>
+                  <ListItem />
+                  <ListItem button>
+                    <ListItemText
+                      disabled
+                      secondary="Beschreibung: Problemanalyse !"
+                    />
+                  </ListItem>
+                </List>
+                <Divider />
+              </Collapse>
+
+              <ListItem
+                button
+                className={classes.nested}
+                onClick={() => this.handleClick('openArbeitsergebnisse')}
+              >
                 <ListItemText secondary="Arbeitsergebnisse" />
-                <ListItemText
-                  className={classes.rightAlignText}
-                  primary="3 / 5"
-                />
+                {this.state.openArbeitsergebnisse ? (
+                  <Icon>expand_less</Icon>
+                ) : (
+                  <Icon>expand_more</Icon>
+                )}
+                <TextField
+                  select
+                  className={classNames(classes.margin, classes.textField)}
+                  value={this.state.arbeitsergebnisse}
+                  onChange={this.handleChange('arbeitsergebnisse')}
+                >
+                  {gradingValues.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
               </ListItem>
-              <ListItem button className={classes.nested}>
+              <Collapse
+                in={this.state.openArbeitsergebnisse}
+                timeout="auto"
+                unmountOnExit
+              >
+                <List
+                  component="div"
+                  disablePadding
+                  className={classes.nestedText}
+                >
+                  <ListItem>
+                    <TextField
+                      id="multiline-flexible"
+                      label="Kommentar"
+                      multiline
+                      fullWidth
+                      rowsMax="4"
+                      value={this.state.multiline}
+                      onChange={this.handleChange('multiline')}
+                      InputProps={{
+                        disableUnderline: true,
+                        classes: {
+                          root: classes.bootstrapRoot,
+                          input: classes.bootstrapInput
+                        }
+                      }}
+                      InputLabelProps={{
+                        shrink: true,
+                        className: classes.bootstrapFormLabel
+                      }}
+                    />
+                  </ListItem>
+                  <ListItem />
+                  <ListItem button>
+                    <ListItemText
+                      disabled
+                      secondary="Beschreibung:  Arbeitsergebnisse
+ !"
+                    />
+                  </ListItem>
+                </List>
+                <Divider />
+              </Collapse>
+              <ListItem
+                button
+                className={classes.nested}
+                onClick={() => this.handleClick('openArbeitsweise')}
+              >
                 <ListItemText secondary="Arbeitsweise" />
-                <ListItemText
-                  className={classes.rightAlignText}
-                  primary="1 / 5"
-                />
+                {this.state.openArbeitsweise ? (
+                  <Icon>expand_less</Icon>
+                ) : (
+                  <Icon>expand_more</Icon>
+                )}
+                <TextField
+                  select
+                  className={classNames(classes.margin, classes.textField)}
+                  value={this.state.arbeitsweise}
+                  onChange={this.handleChange('arbeitsweise')}
+                >
+                  {gradingValues.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
               </ListItem>
+
+              <Collapse
+                in={this.state.openArbeitsweise}
+                timeout="auto"
+                unmountOnExit
+              >
+                <List
+                  component="div"
+                  disablePadding
+                  className={classes.nestedText}
+                >
+                  <ListItem>
+                    <TextField
+                      id="multiline-flexible"
+                      label="Kommentar"
+                      multiline
+                      fullWidth
+                      rowsMax="4"
+                      value={this.state.multiline}
+                      onChange={this.handleChange('multiline')}
+                      InputProps={{
+                        disableUnderline: true,
+                        classes: {
+                          root: classes.bootstrapRoot,
+                          input: classes.bootstrapInput
+                        }
+                      }}
+                      InputLabelProps={{
+                        shrink: true,
+                        className: classes.bootstrapFormLabel
+                      }}
+                    />
+                  </ListItem>
+                  <ListItem />
+                  <ListItem button>
+                    <ListItemText
+                      disabled
+                      secondary="Beschreibung: Arbeitsweise !"
+                    />
+                  </ListItem>
+                </List>
+                <Divider />
+              </Collapse>
             </List>
           </List>
           <Divider />
