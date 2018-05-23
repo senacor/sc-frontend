6127e51d72f24ec9b2fe9e019a2c773904345388
Konrad Ullmann <konrad.ullmann@senacor.com>
2018-05-23 15:52:05 +0200

    PR-259 Bewertungsfelder der Kategorie WirkungImTeam ergänzt mit Tests

--- src/components/pr/PrKommentar.test.js
+++ src/components/pr/PrKommentar.test.js
@@ -7,149 +7,69 @@ describe('PrKommentar Component', () => {
 
   const prById = {
     id: 1,
-    employee: {
-      id: 1,
-      login: 'lschäfer',
-      firstName: 'Lionel',
-      lastName: 'Schäfer',
-      title: 'DR',
-      email: 'lionel.schäfer@senacor.com',
-      entryDate: '2004-05-10',
-      salaries: [
-        {
-          workingHoursPerWeek: 40,
-          ote: 45000,
-          fte: 1,
-          basicSalary: 40500,
-          validFrom: '2004-05-10'
-        },
-        {
-          workingHoursPerWeek: 40,
-          ote: 50000,
-          fte: 1,
-          basicSalary: 40000,
-          validFrom: '2005-05-10'
-        }
-      ],
-      employment: {
-        endOfProbationPeriod: '2004-05-10',
-        jobGradings: [
-          {
-            positionId: 1,
-            salaryLevel: 1,
-            validFrom: '2004-05-09',
-            competence: 'DEVELOPMENT'
-          },
-          {
-            positionId: 2,
-            salaryLevel: 2,
-            validFrom: '2005-05-09',
-            competence: 'DEVELOPMENT'
-          }
-        ],
-        leaves: {
-          unpaidLeave: [
-            {
-              from: '2004-05-10',
-              to: '2005-03-31'
-            }
-          ],
-          maternityLeave: [
-            {
-              from: '2004-05-10',
-              to: '2004-10-11'
-            }
-          ],
-          parentalLeave: [
-            {
-              from: '2004-05-10',
-              to: '2004-08-28'
-            }
-          ],
-          sabbatical: [
-            {
-              from: '2004-05-10',
-              to: '2004-06-07'
-            },
-            {
-              from: '2004-05-10',
-              to: '2005-02-10'
-            },
-            {
-              from: '2004-05-10',
-              to: '2005-03-26'
-            }
-          ]
-        }
-      }
-    },
-    supervisor: 'ttran',
-    occasion: 'ON_DEMAND',
-    status: 'PREPARATION',
-    deadline: '2018-03-14',
     prRatingSet: [
       {
         id: 2,
         prRatingDescription: 'CUSTOMER_RETENTION',
         prRatingCategory: 'IMPACT_ON_CUSTOMER',
-        rating: null,
-        comment: null
+        rating: 1,
+        comment: 'aaa'
       },
       {
         id: 8,
         prRatingDescription: 'FREE_TEXT_FIELD',
         prRatingCategory: 'OVERALL_ASSESSMENT',
-        rating: null,
-        comment: null
+        rating: 2,
+        comment: 'bbb'
       },
       {
         id: 3,
         prRatingDescription: 'WORKING_MANNER',
         prRatingCategory: 'PERFORMANCE_IN_PROJECT',
-        rating: 1,
-        comment: 'nestojjkfgjdkghjrighjfkr'
+        rating: 3,
+        comment: 'ccc'
       },
       {
         id: 1,
         prRatingDescription: 'WORK_RESULTS',
         prRatingCategory: 'PERFORMANCE_IN_PROJECT',
-        rating: 2,
-        comment: 'kköhhjcsss'
+        rating: 4,
+        comment: 'ddd'
       },
       {
         id: 4,
         prRatingDescription: 'LEADERSHIP',
         prRatingCategory: 'IMPACT_ON_TEAM',
-        rating: null,
-        comment: null
+        rating: 5,
+        comment: 'eee'
       },
       {
         id: 9,
         prRatingDescription: 'TEAMWORK',
         prRatingCategory: 'IMPACT_ON_TEAM',
-        rating: null,
-        comment: null
+        rating: 1,
+        comment: 'fff'
       },
       {
         id: 5,
         prRatingDescription: 'PROBLEM_ANALYSIS',
         prRatingCategory: 'PERFORMANCE_IN_PROJECT',
-        rating: 5,
-        comment: 'kvvvuierkfjrfhkrehghfj'
+        rating: 2,
+        comment: 'ggg'
       },
       {
         id: 6,
         prRatingDescription: 'CONTRIBUTION_TO_COMPANY_DEVELOPMENT',
         prRatingCategory: 'IMPACT_ON_COMPANY',
-        rating: null,
-        comment: null
+        rating: 3,
+        comment: 'hhh'
       },
       {
         id: 7,
         prRatingDescription: 'CUSTOMER_INTERACTION',
         prRatingCategory: 'IMPACT_ON_CUSTOMER',
-        rating: null,
-        comment: null
+        rating: 4,
+        comment: 'iii'
       }
     ],
     taskSet: [
@@ -169,9 +89,17 @@ describe('PrKommentar Component', () => {
     }
   };
 
-  it('displays the PrKommentar', () => {
+  it('should display ProblemAnalysis', () => {
     const component = shallow(
       <StyledComponent prById={prById} category="PROBLEM_ANALYSIS" />
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should display Teamwork', () => {
+    const component = shallow(
+      <StyledComponent prById={prById} category="TEAMWORK" />
     );
 
     expect(component).toMatchSnapshot();
