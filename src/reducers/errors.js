54136eabe6b0bc34292dd1019fa2bf555e2e6eae
Andreas Kremsner <andreas.kremsner@senacor.com>
2018-07-03 13:46:52 +0200

    PR 279 No errors for wrong pr delegation Remove case for generating dedicated error response

--- src/reducers/errors.js
+++ src/reducers/errors.js
@@ -1,4 +1,4 @@
-import { ERROR_RESPONSE, ERROR_RESPONSE_DELEGATION, ERROR_GONE } from '../helper/dispatchTypes';
+import { ERROR_RESPONSE, ERROR_GONE } from '../helper/dispatchTypes';
 
 const defaultState = { hasErrors: false, message: null };
 
@@ -6,8 +6,6 @@ const errors = (state = defaultState, action) => {
   switch (action.type) {
     case ERROR_RESPONSE:
       return generateErrorState(action.httpCode);
-    case ERROR_RESPONSE_DELEGATION:
-      return generateErrorState(action.httpCode, action.errorMessage);
     case ERROR_GONE:
       return defaultState;
     default:
@@ -16,10 +14,6 @@ const errors = (state = defaultState, action) => {
 };
 
 function generateErrorState(httpCode, errorMessage, showErrorMessage) {
-  //Todo: Methode umschreiben, vielleicht Warnungen/Hinweise ausgeben in anderer Farbe als rot
-  if (httpCode === 400) {
-    return Object.assign({}, { hasErrors: true, message: errorMessage });
-  }
   if (httpCode < 500) {
     return Object.assign({}, { hasErrors: false, message: null });
   }
