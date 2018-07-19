34074028fddbae8c8ca00d0c2cd32ff088def769
mfbieber <mfbieber@gmail.com>
2018-07-19 15:07:27 +0200

    Unit test attempt (as comment) for DatePicker, cannot test if changedDate is called onChange.

--- src/components/availabilityView/DatePicker.test.js
+++ src/components/availabilityView/DatePicker.test.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { StyledComponent as DatePicker } from './DatePicker';
+import { StyledComponent } from './DatePicker';
 
 describe('DatePicker', () => {
-  it('should match snapshot', () => {
-    let cut = shallow(<DatePicker />);
+  /*The following attempt leads to a failing test saying:
+  "TypeError: this.props.changeDate is not a function"
 
-    expect(cut).toMatchSnapshot();
+  it('should call changeDate upon picking a date', wait => {
+    const handleTimeChange = props => {
+      console.log(props.changeDate);
+      expect(props.changeDate);
+      wait();
+    };
+    const props = {
+      changeDate: jest.fn()
+    };
+    const wrapper = shallow(
+      <StyledComponent props={props} onChange={handleTimeChange} />
+    ).dive();
+    console.log(wrapper.debug());
+    wrapper.find('TextField').simulate('change', { target: '2018-03-08' });
+  });*/
+
+  it('should match snapshot', () => {
+    let component = shallow(<StyledComponent />);
+
+    expect(component).toMatchSnapshot();
   });
 });
