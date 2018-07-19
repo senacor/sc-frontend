c30c68a6b388c6db6752375f552aa3d9cd477983
mfbieber <mfbieber@gmail.com>
2018-07-19 17:11:18 +0200

    WIP: Unit test attempt (as comment) for DatePicker, cannot test if changedDate is called onChange. WIP also for TimeTable logic.

--- src/components/availabilityView/DatePicker.test.js
+++ src/components/availabilityView/DatePicker.test.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { StyledComponent } from './DatePicker';
+import { changeDate } from '../../actions/appointments';
 
 describe('DatePicker', () => {
   /*The following attempt leads to a failing test saying:
-  "TypeError: this.props.changeDate is not a function"
+  "TypeError: Cannot read property '_isMockFunction' of undefined"
 
-  it('should call changeDate upon picking a date', wait => {
-    const handleTimeChange = props => {
-      console.log(props.changeDate);
-      expect(props.changeDate);
-      wait();
+  it('should call changeDate upon picking a date', () => {
+    const handleTimeChange = () => {
+      expect(mockChangeDate).toBeCalled();
     };
+    const mockChangeDate = jest.spyOn(changeDate, 'dispatch');
     const props = {
-      changeDate: jest.fn()
+      changeDate: changeDate()
     };
     const wrapper = shallow(
-      <StyledComponent props={props} onChange={handleTimeChange} />
+      <StyledComponent onChange={handleTimeChange} />
     ).dive();
-    console.log(wrapper.debug());
+    wrapper.setProps(props);
+    console.log(wrapper.instance());
     wrapper.find('TextField').simulate('change', { target: '2018-03-08' });
+    expect(mockChangeDate).toBeCalled();
   });*/
 
   it('should match snapshot', () => {
--- src/components/availabilityView/TimeTable.test.js
+++ src/components/availabilityView/TimeTable.test.js
@@ -2,10 +2,38 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { StyledComponent as TimeTable } from './TimeTable';
 
-describe('DatePicker', () => {
-  it('should match snapshot', () => {
-    let cut = shallow(<TimeTable />);
+const timeTableListHeight = 25;
+const firstHourOfDay = 8;
+const lastHourOfDay = 19;
+const minuteGranularity = 30;
+const timeTableHours = lastHourOfDay - firstHourOfDay;
 
-    expect(cut).toMatchSnapshot();
+describe('DatePicker', () => {
+  it('excludes appointments that are completely before or after the time window', () => {
+    const props = {
+      selectedDate: '2018-03-08'
+    };
+    const component = shallow(<TimeTable />).dive();
+    console.log(component.instance());
+    const appointmentsFilterMock = appointments => {
+      return component.instance().appointmentsFilter(appointments);
+    };
+    let startAppointment = '2018-03-08T10:00Z[UTC]';
+    let endAppointment = '2018-03-08T23:00Z[UTC]';
+    let appointment = [];
+    appointment.push(startAppointment);
+    appointment.push(endAppointment);
+    console.log(appointment);
+    let appointments = [];
+    appointments.push(appointment);
+    console.log(appointments);
+    console.log(component.instance());
+    expect(appointmentsFilterMock(appointments)).toEqual([]);
+  });
+
+  it('should match snapshot', () => {
+    let component = shallow(<TimeTable />);
+
+    expect(component).toMatchSnapshot();
   });
 });
