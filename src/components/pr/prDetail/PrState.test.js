fe3763ce682f9df23c9b960b8ca287055c7681d6
Helena Henkel <Helena.Henkel@senacor.com>
2018-10-09 14:48:21 +0200

    complete test for employee and HR
    employee should see 3 steps and be able to click buttons
    HR should see 4 steps and not be able to click buttons (we do not check last two items of reviewer and employee)

--- src/components/pr/prDetail/PrState.test.js
+++ src/components/pr/prDetail/PrState.test.js
@@ -5,7 +5,7 @@ import { prStatusEnum } from '../../../helper/prStatus';
 import PrStatusActionButton from './PrStatusActionButton';
 import PrStateStepper from './PrStateStepper';
 
-describe('PrState Component', () => {
+describe('PrState Component for reviewer', () => {
   let shallow = createShallow({ dive: true });
 
   const releaseButtonClickMock = jest.fn();
@@ -225,5 +225,427 @@ describe('PrState Component', () => {
 
     expect(addPrStatusMock.mock.calls.length).toBe(1);
     expect(addPrStatusMock.mock.calls[0][0]).toBe(prById);
+  });
+});
+
+describe('PrState Component for employee', () => {
+  let shallow = createShallow({ dive: true });
+
+  const releaseButtonClickMock = jest.fn();
+
+  const prStatusesDone = {
+    [prStatusEnum.RELEASED_SHEET_EMPLOYEE]: true,
+    [prStatusEnum.RELEASED_SHEET_REVIEWER]: false,
+    [prStatusEnum.FIXED_DATE]: false,
+    [prStatusEnum.FINALIZED_REVIEWER]: false,
+    [prStatusEnum.FINALIZED_EMPLOYEE]: false,
+    [prStatusEnum.ARCHIVED_HR]: false
+  };
+
+  const stepStructure = [
+    {
+      mainStepLabel: 'Vorbereitung',
+      substeps: {
+        [prStatusEnum.RELEASED_SHEET_EMPLOYEE]: {
+          isCompleted: true,
+          isCurrentUserActionPerformer: true,
+          label: 'Mitarbeiter: ',
+          rendering: {
+            complete: 'Abgeschlossen',
+            incompleteForNonActionPerformer: 'Nicht abgeschlossen',
+            incompleteForActionPerformer: (
+              <PrStatusActionButton
+                label={'Freigabe'}
+                releaseButtonClick={releaseButtonClickMock(
+                  prStatusEnum.RELEASED_SHEET_EMPLOYEE
+                )}
+              />
+            )
+          }
+        },
+        [prStatusEnum.RELEASED_SHEET_REVIEWER]: {
+          isCompleted: false,
+          isCurrentUserActionPerformer: false,
+          label: 'Beurteiler: ',
+          rendering: {
+            complete: 'Abgeschlossen',
+            incompleteForNonActionPerformer: 'Nicht abgeschlossen',
+            incompleteForActionPerformer: (
+              <PrStatusActionButton
+                label={'Freigabe'}
+                releaseButtonClick={releaseButtonClickMock(
+                  prStatusEnum.RELEASED_SHEET_REVIEWER
+                )}
+              />
+            )
+          }
+        },
+        [prStatusEnum.FIXED_DATE]: {
+          isCompleted: false,
+          isCurrentUserActionPerformer: true,
+          label: 'Terminfindung',
+          rendering: {
+            complete: 'Alle Teilnehmer haben zugesagt',
+            incompleteForNonActionPerformer: 'Nicht abgeschlossen',
+            incompleteForActionPerformer: 'Nicht abgeschlossen'
+          }
+        }
+      }
+    },
+    {
+      mainStepLabel: 'Gespräch',
+      substeps: {
+        [prStatusEnum.FINALIZED_REVIEWER]: {
+          isCompleted: false,
+          isCurrentUserActionPerformer: false,
+          label: 'Beurteiler: ',
+          rendering: {
+            complete: <div>Abgeschlossen</div>,
+            incompleteForNonActionPerformer: 'Nicht abgeschlossen',
+            incompleteForActionPerformer: (
+              <PrStatusActionButton
+                label={'Freigabe'}
+                releaseButtonClick={releaseButtonClickMock(
+                  prStatusEnum.FINALIZED_REVIEWER
+                )}
+              />
+            )
+          }
+        }
+      }
+    },
+    {
+      mainStepLabel: 'Abschluss',
+      substeps: {
+        [prStatusEnum.FINALIZED_EMPLOYEE]: {
+          isCompleted: false,
+          isCurrentUserActionPerformer: true,
+          label: 'Mitarbeiter:',
+          rendering: {
+            complete: 'Abgeschlossen',
+            incompleteForNonActionPerformer: 'Nicht abgeschlossen',
+            incompleteForActionPerformer: (
+              <PrStatusActionButton
+                label={'Freigabe'}
+                releaseButtonClick={releaseButtonClickMock(
+                  prStatusEnum.FINALIZED_EMPLOYEE
+                )}
+              />
+            )
+          }
+        }
+      }
+    }
+  ];
+
+  const prStatuses = [prStatusEnum.RELEASED_SHEET_EMPLOYEE];
+
+  const userinfo = {
+    userPrincipalName: 'test.pr.mitarbeiter1'
+  };
+
+  it('should match snapshot', () => {
+    const component = shallow(<StyledComponent />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    const prById = {
+      statuses: prStatusEnum.RELEASED_SHEET_EMPLOYEE
+    };
+
+    const component = shallow(
+      <StyledComponent prById={prById} userinfo={userinfo} />
+    );
+
+    expect(component.find(PrStateStepper)).toHaveLength(1);
+    expect(component.find(PrStateStepper).props().activeStep).toEqual(0);
+    expect(
+      component.find(PrStateStepper).props().stepStructure[0].mainStepLabel
+    ).toEqual('Vorbereitung');
+  });
+
+  it('should call mainStepIsDone which returns the complete status of the mainStep', () => {
+    const component = shallow(<StyledComponent />);
+    const instance = component.instance();
+
+    const result = instance.mainStepIsDone(prStatusesDone, 1, stepStructure);
+    expect(result).toBeFalsy();
+  });
+
+  it('should call calculateActiveStep which calculates the active step', () => {
+    const component = shallow(<StyledComponent />);
+    const instance = component.instance();
+
+    const result = instance.calculateActiveStep(prStatusesDone, stepStructure);
+    expect(result).toBe(0);
+  });
+
+  it('should call getCompletedSubsteps which returns an object with the status substeps', () => {
+    const component = shallow(<StyledComponent />);
+    const instance = component.instance();
+
+    const result = instance.getCompletedSubsteps(prStatuses);
+    expect(result).toEqual(prStatusesDone);
+  });
+
+  it('should call isDone which checks for a single status whether it is completed', () => {
+    const component = shallow(<StyledComponent />);
+    const instance = component.instance();
+
+    const result = instance.isDone(prStatuses, [
+      prStatusEnum.FINALIZED_EMPLOYEE
+    ]);
+    expect(result).toBeFalsy();
+  });
+
+  it('should call updateStepStructure which returns the updated stepStructure', () => {
+    const prById = {
+      id: 1,
+      employee: {
+        login: 'test.pr.mitarbeiter1'
+      },
+      supervisor: {
+        login: 'ttran'
+      },
+      reviewer: {
+        login: 'test.pr.beurteiler'
+      }
+    };
+
+    const component = shallow(<StyledComponent />);
+    const instance = component.instance();
+
+    const result = instance.updateStepStructure(
+      prById,
+      userinfo,
+      prStatusesDone,
+      releaseButtonClickMock
+    );
+    expect(result).toEqual(stepStructure);
+  });
+
+  it('should call releaseButtonClick which triggers the the addPrStatus action', () => {
+    const addPrStatusMock = jest.fn();
+    const mockedEvent = { disabled: false };
+    const prById = {
+      id: 1
+    };
+
+    const component = shallow(
+      <StyledComponent
+        prById={prById}
+        addPrStatus={addPrStatusMock}
+        userinfo={userinfo}
+      />
+    );
+    const instance = component.instance();
+
+    let result = instance.releaseButtonClick(
+      prStatusEnum.RELEASED_SHEET_EMPLOYEE
+    );
+    result(mockedEvent);
+
+    expect(addPrStatusMock.mock.calls.length).toBe(1);
+    expect(addPrStatusMock.mock.calls[0][0]).toBe(prById);
+  });
+});
+
+describe('PrState Component for HR', () => {
+  let shallow = createShallow({ dive: true });
+
+  const releaseButtonClickMock = jest.fn();
+
+  const prStatusesDone = {
+    [prStatusEnum.RELEASED_SHEET_EMPLOYEE]: true,
+    [prStatusEnum.RELEASED_SHEET_REVIEWER]: false,
+    [prStatusEnum.FIXED_DATE]: false,
+    [prStatusEnum.FINALIZED_REVIEWER]: false,
+    [prStatusEnum.FINALIZED_EMPLOYEE]: false,
+    [prStatusEnum.ARCHIVED_HR]: false
+  };
+
+  const stepStructure = [
+    {
+      mainStepLabel: 'Vorbereitung',
+      substeps: {
+        [prStatusEnum.RELEASED_SHEET_EMPLOYEE]: {
+          isCompleted: true,
+          isCurrentUserActionPerformer: false,
+          label: 'Mitarbeiter: ',
+          rendering: {
+            complete: 'Abgeschlossen',
+            incompleteForNonActionPerformer: 'Nicht abgeschlossen',
+            incompleteForActionPerformer: (
+              <PrStatusActionButton
+                label={'Freigabe'}
+                releaseButtonClick={releaseButtonClickMock(
+                  prStatusEnum.RELEASED_SHEET_EMPLOYEE
+                )}
+              />
+            )
+          }
+        },
+        [prStatusEnum.RELEASED_SHEET_REVIEWER]: {
+          isCompleted: false,
+          isCurrentUserActionPerformer: false,
+          label: 'Beurteiler: ',
+          rendering: {
+            complete: 'Abgeschlossen',
+            incompleteForNonActionPerformer: 'Nicht abgeschlossen',
+            incompleteForActionPerformer: (
+              <PrStatusActionButton
+                label={'Freigabe'}
+                releaseButtonClick={releaseButtonClickMock(
+                  prStatusEnum.RELEASED_SHEET_REVIEWER
+                )}
+              />
+            )
+          }
+        },
+        [prStatusEnum.FIXED_DATE]: {
+          isCompleted: false,
+          isCurrentUserActionPerformer: false,
+          label: 'Terminfindung',
+          rendering: {
+            complete: 'Alle Teilnehmer haben zugesagt',
+            incompleteForNonActionPerformer: 'Nicht abgeschlossen',
+            incompleteForActionPerformer: 'Nicht abgeschlossen'
+          }
+        }
+      }
+    },
+    {
+      mainStepLabel: 'Gespräch',
+      substeps: {
+        [prStatusEnum.FINALIZED_REVIEWER]: {
+          isCompleted: false,
+          isCurrentUserActionPerformer: false,
+          label: 'Beurteiler: ',
+          rendering: {
+            complete: <div>Abgeschlossen</div>,
+            incompleteForNonActionPerformer: 'Nicht abgeschlossen',
+            incompleteForActionPerformer: (
+              <PrStatusActionButton
+                label={'Freigabe'}
+                releaseButtonClick={releaseButtonClickMock(
+                  prStatusEnum.FINALIZED_REVIEWER
+                )}
+              />
+            )
+          }
+        }
+      }
+    },
+    {
+      mainStepLabel: 'Abschluss',
+      substeps: {
+        [prStatusEnum.FINALIZED_EMPLOYEE]: {
+          isCompleted: false,
+          isCurrentUserActionPerformer: false,
+          label: 'Mitarbeiter:',
+          rendering: {
+            complete: 'Abgeschlossen',
+            incompleteForNonActionPerformer: 'Nicht abgeschlossen',
+            incompleteForActionPerformer: (
+              <PrStatusActionButton
+                label={'Freigabe'}
+                releaseButtonClick={releaseButtonClickMock(
+                  prStatusEnum.FINALIZED_EMPLOYEE
+                )}
+              />
+            )
+          }
+        }
+      }
+    },
+    {
+      mainStepLabel: 'Archivieren',
+      substeps: {
+        [prStatusEnum.ARCHIVED_HR]: {
+          isCompleted: false,
+          label: 'HR: ',
+          rendering: {
+            complete: 'Archiviert',
+            incompleteForNonActionPerformer: 'Nicht abgeschlossen',
+            incompleteForActionPerformer: (
+              <PrStatusActionButton
+                label={'Freigabe'}
+                releaseButtonClick={releaseButtonClickMock(
+                  prStatusEnum.ARCHIVED_HR
+                )}
+              />
+            )
+          }
+        }
+      }
+    }
+  ];
+
+  const prStatuses = [prStatusEnum.RELEASED_SHEET_EMPLOYEE];
+
+  const userinfo = {
+    userPrincipalName: 'test.pr.hr'
+  };
+
+  const userroles = ['PR_HR'];
+
+  it('should match snapshot', () => {
+    const component = shallow(<StyledComponent />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    const prById = {
+      statuses: prStatusEnum.RELEASED_SHEET_EMPLOYEE
+    };
+
+    const component = shallow(
+      <StyledComponent
+        prById={prById}
+        userinfo={userinfo}
+        userroles={userroles}
+      />
+    );
+
+    expect(component.find(PrStateStepper)).toHaveLength(1);
+    expect(component.find(PrStateStepper).props().activeStep).toEqual(0);
+    expect(
+      component.find(PrStateStepper).props().stepStructure[0].mainStepLabel
+    ).toEqual('Vorbereitung');
+  });
+
+  it('should call mainStepIsDone which returns the complete status of the mainStep', () => {
+    const component = shallow(<StyledComponent />);
+    const instance = component.instance();
+
+    const result = instance.mainStepIsDone(prStatusesDone, 1, stepStructure);
+    expect(result).toBeFalsy();
+  });
+
+  it('should call calculateActiveStep which calculates the active step', () => {
+    const component = shallow(<StyledComponent />);
+    const instance = component.instance();
+
+    const result = instance.calculateActiveStep(prStatusesDone, stepStructure);
+    expect(result).toBe(0);
+  });
+
+  it('should call getCompletedSubsteps which returns an object with the status substeps', () => {
+    const component = shallow(<StyledComponent />);
+    const instance = component.instance();
+
+    const result = instance.getCompletedSubsteps(prStatuses);
+    expect(result).toEqual(prStatusesDone);
+  });
+
+  it('should call isDone which checks for a single status whether it is completed', () => {
+    const component = shallow(<StyledComponent />);
+    const instance = component.instance();
+
+    const result = instance.isDone(prStatuses, [
+      prStatusEnum.FINALIZED_EMPLOYEE
+    ]);
+    expect(result).toBeFalsy();
   });
 });
