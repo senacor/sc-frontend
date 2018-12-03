2414078dd62bbce316a20a9d56e01bc361ee0cd3
Helena Henkel <helena.henkel@senacor.com>
2018-12-03 16:10:23 +0100

    reset state of required fields (#191)

--- src/reducers/requiredFields.js
+++ src/reducers/requiredFields.js
@@ -7,6 +7,12 @@ export const requiredFields = (
   switch (action.type) {
     case dispatchTypes.REQUIRED_FIELDS:
       return action.payload;
+    case dispatchTypes.REVIEWER_INFO_RESPONSE:
+      return { employee: true, reviewer: true };
+    case dispatchTypes.SET_PR_TAB:
+      return { employee: true, reviewer: true };
+    case dispatchTypes.ADD_PR_STATUS_RESPONSE:
+      return { employee: true, reviewer: true };
     default:
       return state;
   }
