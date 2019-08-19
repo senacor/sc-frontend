f51c533addcd9df9d763a615233ae041b7e79d27
mpiroh <matus.piroh@senacor.com>
2019-08-19 11:49:44 +0200

    PR-868: working on PrState

--- src/actions/calls/userinfo.js
+++ src/actions/calls/userinfo.js
@@ -22,7 +22,7 @@ export const getUserInfo = async (userinfoContext, authorizationContext) => {
   if (response.ok) {
     rolesResponse = await response.json();
     //ROLEHACK: HR
-    rolesResponse.value[0].displayName = 'PR_HR';
+    rolesResponse.value[0].displayName = 'PR_Mitarbeiter';
     //rolesResponse.value[0].displayName = 'PR_CST_Leiter';
     //rolesResponse.value[0].displayName = 'PR_HR';
   } else {
