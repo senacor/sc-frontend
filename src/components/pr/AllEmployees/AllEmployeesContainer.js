c4fe8f379fa990b6c791f66ec4dbdfaff767313c
mpiroh <matus.piroh@senacor.com>
2019-09-24 12:59:04 +0200

    feature/PR-956: upload button added

--- src/components/pr/AllEmployees/AllEmployeesContainer.js
+++ src/components/pr/AllEmployees/AllEmployeesContainer.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useContext, useState, Fragment } from 'react';
 import { injectIntl } from 'react-intl';
-import { withStyles } from '@material-ui/core';
-
-// Components
-import AllEmployeesFilter from './AllEmployeesFilter';
-
-// Material UI
+import { Button, TextField, withStyles } from '@material-ui/core';
+import { ErrorContext, UserinfoContext } from '../../App';
+import { uploadFiles } from '../../../actions/calls/fileStorage';
 import EmployeesGrid from './AllEmployeesGrid';
+import ROLES from '../../../helper/roles';
+import AllEmployeesFilter from './AllEmployeesFilter';
+import UploadSuccessDialog from '../../fileStorage/UploadSuccessDialog';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
   container: {
@@ -20,19 +21,81 @@ const styles = theme => ({
   }
 });
 
-const AllEmployeesContainer = ({ classes }) => {
+const AllEmployeesContainer = ({ classes, intl }) => {
   const [searchEmployeesValue, setSearchEmployeesValue] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+
+  const userInfoContext = useContext(UserinfoContext.context);
+  const errorContext = useContext(ErrorContext.context);
 
   const handleSearchChange = event => {
     setSearchEmployeesValue(event.target.value);
   };
 
+  const checkFileForm = file => {
+    const regex = /^20[0-9]{6}_\D{3}/g;
+    if (file.match(regex)) {
+      return true;
+    } else {
+      errorContext.setValue({
+        hasErrors: true,
+        messageId: 'message.uploadError'
+      });
+    }
+  };
+
+  const handleChange = event => {
+    if (checkFileForm(event.target.files[0].name)) {
+      uploadFiles(
+        event.target.files,
+        setUploadedFiles,
+        setIsLoading,
+        errorContext
+      );
+    }
+  };
+
+  const handleClose = () => {
+    setUploadedFiles([]);
+  };
+
   return (
     <div className={classes.container}>
+      <UploadSuccessDialog
+        open={uploadedFiles.length > 0}
+        onClose={handleClose}
+        uploadedFiles={uploadedFiles}
+      />
       <AllEmployeesFilter
         searchValue={searchEmployeesValue}
         searchChange={handleSearchChange}
       />
+      {userInfoContext.value.userroles.includes(ROLES.PERSONAL_DEV) && (
+        <Fragment>
+          <TextField
+            style={{ display: 'none' }}
+            id="upload-button"
+            multiple
+            type="file"
+            onChange={handleChange}
+          />
+          <label htmlFor="upload-button">
+            <Button component="span">
+              {isLoading ? (
+                <CircularProgress
+                  size={24}
+                  className={classes.buttonProgress}
+                />
+              ) : (
+                intl.formatMessage({
+                  id: 'allemployeescontainer.upload'
+                })
+              )}
+            </Button>
+          </label>
+        </Fragment>
+      )}
       <EmployeesGrid searchEmployeesValue={searchEmployeesValue} />
     </div>
   );
