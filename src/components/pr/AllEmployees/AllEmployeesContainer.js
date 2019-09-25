1a28dc2a143ed01b5344f07c9e8b4fc4aac1afeb
Pillár <Miroslav.Pillar@senacor.com>
2019-09-25 08:55:13 +0200

    styling upload button

--- src/components/pr/AllEmployees/AllEmployeesContainer.js
+++ src/components/pr/AllEmployees/AllEmployeesContainer.js
@@ -13,6 +13,15 @@ const styles = theme => ({
   container: {
     margin: 3 * theme.spacing.unit
   },
+  filterWithUpload: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0 3rem'
+  },
+  btnUpload: {
+    border: `1px solid ${theme.palette.secondary.grey}`
+  },
   gridContainer: {
     height: '77vh',
     width: '100%',
@@ -62,40 +71,42 @@ const AllEmployeesContainer = ({ classes, intl }) => {
 
   return (
     <div className={classes.container}>
-      <UploadSuccessDialog
-        open={uploadedFiles.length > 0}
-        onClose={handleClose}
-        uploadedFiles={uploadedFiles}
-      />
-      <AllEmployeesFilter
-        searchValue={searchEmployeesValue}
-        searchChange={handleSearchChange}
-      />
-      {userInfoContext.value.userroles.includes(ROLES.PERSONAL_DEV) && (
-        <Fragment>
-          <TextField
-            style={{ display: 'none' }}
-            id="upload-button"
-            multiple
-            type="file"
-            onChange={handleChange}
-          />
-          <label htmlFor="upload-button">
-            <Button component="span">
-              {isLoading ? (
-                <CircularProgress
-                  size={24}
-                  className={classes.buttonProgress}
-                />
-              ) : (
-                intl.formatMessage({
-                  id: 'allemployeescontainer.upload'
-                })
-              )}
-            </Button>
-          </label>
-        </Fragment>
-      )}
+      <div className={classes.filterWithUpload}>
+        <UploadSuccessDialog
+          open={uploadedFiles.length > 0}
+          onClose={handleClose}
+          uploadedFiles={uploadedFiles}
+        />
+        <AllEmployeesFilter
+          searchValue={searchEmployeesValue}
+          searchChange={handleSearchChange}
+        />
+        {userInfoContext.value.userroles.includes(ROLES.PERSONAL_DEV) && (
+          <Fragment>
+            <TextField
+              style={{ display: 'none' }}
+              id="upload-button"
+              multiple
+              type="file"
+              onChange={handleChange}
+            />
+            <label htmlFor="upload-button">
+              <Button component="span" className={classes.btnUpload}>
+                {isLoading ? (
+                  <CircularProgress
+                    size={24}
+                    className={classes.buttonProgress}
+                  />
+                ) : (
+                  intl.formatMessage({
+                    id: 'allemployeescontainer.upload'
+                  })
+                )}
+              </Button>
+            </label>
+          </Fragment>
+        )}
+      </div>
       <EmployeesGrid searchEmployeesValue={searchEmployeesValue} />
     </div>
   );
