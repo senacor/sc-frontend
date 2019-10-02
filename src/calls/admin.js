4eb6e0b4a44f76869f44cea9e69563b022de0f80
mpiroh <matus.piroh@senacor.com>
2019-10-02 15:41:55 +0200

    feature/PR-965-feedbacks: feedback panel implemented

--- src/calls/admin.js
+++ src/calls/admin.js
@@ -120,3 +120,37 @@ export const deleteError = async (id, errorContext) => {
     });
   }
 };
+
+export const getFeedbacks = async (setData, setIsLoading, errorContext) => {
+  try {
+    setIsLoading(true);
+
+    const response = await fetch(`${process.env.REACT_APP_API}/api/v3/feedback/all`);
+    const data = await response.json();
+
+    setData(data);
+    setIsLoading(false);
+  } catch (err) {
+    console.log(err);
+    errorContext.setValue({
+      hasErrors: true,
+      messageId: 'message.error'
+    });
+  }
+};
+
+export const deleteFeedbacks = async (ids, errorContext) => {
+  try {
+    await fetch(`${process.env.REACT_APP_API}/api/v3/feedback`, {
+      method: 'delete',
+      mode: 'cors',
+      body: JSON.stringify(ids)
+    });
+  } catch (err) {
+    console.log(err);
+    errorContext.setValue({
+      hasErrors: true,
+      messageId: 'message.error'
+    });
+  }
+};
---
+++ src/components/admin/Feedbacks.js
@@ -0,0 +1,162 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { injectIntl } from 'react-intl';
+import { CircularProgress, withStyles } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { deleteFeedbacks, getFeedbacks } from '../../calls/admin';
+import { getReadableDate } from '../../helper/date';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { ErrorContext } from '../App';
+
+const styles = theme => ({
+  spacing: {
+    margin: 3 * theme.spacing.unit
+  },
+  padding: {
+    paddingTop: 3 * theme.spacing.unit,
+    paddingLeft: 3 * theme.spacing.unit
+  }
+});
+
+export const Feedbacks = ({ classes, intl }) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [page, setPage] = useState(0);
+
+  let errorContext = useContext(ErrorContext.context);
+
+  useEffect(() => {
+    getFeedbacks(setData, setIsLoading, errorContext);
+  }, []);
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(event.target.value);
+  };
+
+  const handleChangePage = (event, page) => {
+    setPage(page);
+  };
+
+  const handleOnDeleteClick = (event, ids) => {
+    deleteFeedbacks(ids, errorContext);
+    let newData = data.filter(entry => !ids.includes(entry.id));
+    setData(newData);
+  };
+
+  return (
+    <div>
+      {isLoading ? (
+        <CircularProgress className={classes.spacing} />
+      ) : (
+        <div>
+          <Paper className={classes.spacing}>
+            <Typography className={classes.padding} variant="h5">
+              {intl.formatMessage({
+                id: 'maintenance.feedbacks'
+              })}
+            </Typography>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    {`${intl.formatMessage({
+                      id: 'maintenance.sentby'
+                    })}`}
+                  </TableCell>
+                  <TableCell>
+                    {`${intl.formatMessage({
+                      id: 'maintenance.type'
+                    })}`}
+                  </TableCell>
+                  <TableCell>
+                    {`${intl.formatMessage({
+                      id: 'maintenance.subject'
+                    })}`}
+                  </TableCell>
+                  <TableCell>
+                    {`${intl.formatMessage({
+                      id: 'maintenance.message'
+                    })}`}
+                  </TableCell>
+                  <TableCell>
+                    {`${intl.formatMessage({
+                      id: 'maintenance.date'
+                    })}`}
+                  </TableCell>
+                  <TableCell>
+                    {`${intl.formatMessage({
+                      id: 'maintenance.delete'
+                    })}`}
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((entry, index) => {
+                    return (
+                      <TableRow key={index}>
+                        <TableCell>{`${entry.senderfirstName} ${
+                          entry.senderLastName
+                        }`}</TableCell>
+                        <TableCell>{entry.context}</TableCell>
+                        <TableCell>{entry.subject}</TableCell>
+                        <TableCell>{entry.body}</TableCell>
+                        <TableCell>{getReadableDate(entry.sentAt)}</TableCell>
+                        <TableCell>
+                          <IconButton
+                            onClick={event =>
+                              handleOnDeleteClick(event, [entry.id])
+                            }
+                            aria-label="delete"
+                          >
+                            <DeleteIcon />
+                          </IconButton>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+            <TablePagination
+              component="div"
+              count={data.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              backIconButtonProps={{
+                'aria-label': `${intl.formatMessage({
+                  id: 'performancereviewtable.previouspage'
+                })}`
+              }}
+              nextIconButtonProps={{
+                'aria-label': `${intl.formatMessage({
+                  id: 'performancereviewtable.nextpage'
+                })}`
+              }}
+              labelRowsPerPage={intl.formatMessage({
+                id: 'performancereviewtable.rowsperpage'
+              })}
+              labelDisplayedRows={({ from, to, count }) =>
+                `${from}-${to} ${intl.formatMessage({
+                  id: 'performancereviewtable.from'
+                })} ${count}`
+              }
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+          </Paper>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default injectIntl(withStyles(styles)(Feedbacks));
