6001b646af20c1ea0cf2f985ec7d1ffba25011ab
smarcin <stefan.marcin@senacor.com>
2019-10-07 15:27:17 +0200

    PR-986: comments considered.

--- src/calls/excelView.js
+++ src/calls/excelView.js
@@ -4,9 +4,7 @@ export const downloadExcel = async employeeIds => {
   let query = employeeIds.join();
 
   const response = await fetch(
-    `${
-      process.env.REACT_APP_API
-    }/api/v3/hr/overview/export?employeeIds=${query}`
+    `${process.env.REACT_APP_API}/api/v3/export?employeeIds=${query}`
   );
   if (response.ok) {
     const blob = await response.blob();
