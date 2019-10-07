6091c09b0eee6aa46c4ea42b34f88136d8f6bf7e
Pillár <Miroslav.Pillar@senacor.com>
2019-10-07 08:58:12 +0200

    styling table head

--- src/components/AllEmployees/AllEmployeesTable/AllEmployeesTableHead.js
+++ src/components/AllEmployees/AllEmployeesTable/AllEmployeesTableHead.js
@@ -7,43 +7,52 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 
-const styles = theme => ({});
+const styles = theme => ({
+  tableHead: {
+    height: 80,
+    backgroundColor: theme.palette.secondary.brightGrey,
+    color: theme.palette.secondary.black
+  },
+  tableCell: {
+    fontSize: '0.8rem'
+  }
+});
 
 const AllEmployeesTableHead = ({ intl, classes }) => {
   return (
     <TableHead>
-      <TableRow>
-        <TableCell>
+      <TableRow className={classes.tableHead}>
+        <TableCell className={classes.tableCell}>
           {intl.formatMessage({
             id: 'employeeInfo.name'
           })}
         </TableCell>
-        <TableCell>
+        <TableCell className={classes.tableCell}>
           {intl.formatMessage({
             id: 'employeeInfo.position'
           })}
         </TableCell>
-        <TableCell>
+        <TableCell className={classes.tableCell}>
           {intl.formatMessage({
             id: 'employeeInfo.cst'
           })}
         </TableCell>
-        <TableCell>
+        <TableCell className={classes.tableCell}>
           {intl.formatMessage({
             id: 'employeeInfo.supervisor'
           })}
         </TableCell>
-        <TableCell>
+        <TableCell className={classes.tableCell}>
           {intl.formatMessage({
             id: 'employeeInfo.cc'
           })}
         </TableCell>
-        <TableCell>
+        <TableCell className={classes.tableCell}>
           {intl.formatMessage({
             id: 'employeeInfo.location'
           })}
         </TableCell>
-        <TableCell>
+        <TableCell className={classes.tableCell}>
           {intl.formatMessage({
             id: 'employeeInfo.startDate'
           })}
