55c4281a1fd8d18b64baa97f615c2007d08bb198
Juraj Bonk <Juraj.Bonk@senacor.com>
2019-10-08 09:57:01 +0200

    Maintenance table warning fix, target role radio button fix, PrDetailInformation null date fix, feedback button styling

--- src/components/sidebar/FeedbackButton.js
+++ src/components/sidebar/FeedbackButton.js
@@ -1,13 +1,20 @@
 import React, { useState, Fragment } from 'react';
 import { injectIntl } from 'react-intl';
 import { withStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
 import FeedbackIcon from '@material-ui/icons/Feedback';
 import FeedbackCreateDialog from './FeedbackCreateDialog';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import List from '@material-ui/core/List';
 
 const styles = theme => ({
-  feedbackIcon: {
-    marginRight: theme.spacing.unit
+  noTextDecoration: {
+    textDecoration: 'none',
+    cursor: 'pointer'
+  },
+  textColor: {
+    color: theme.palette.primary['900']
   }
 });
 
@@ -24,12 +31,23 @@ const FeedbackButton = ({ classes, intl }) => {
 
   return (
     <Fragment>
-      <Button onClick={handleClick}>
-        <FeedbackIcon className={classes.feedbackIcon} />
-        {intl.formatMessage({
-          id: 'sidebar.feedback'
-        })}
-      </Button>
+      <List component="nav">
+        <ListItem className={classes.noTextDecoration} onClick={handleClick}>
+          <ListItemIcon>
+            <FeedbackIcon />
+          </ListItemIcon>
+          <ListItemText
+            disableTypography
+            primary={
+              <div className={classes.textColor}>
+                {intl.formatMessage({
+                  id: 'sidebar.feedback'
+                })}
+              </div>
+            }
+          />
+        </ListItem>
+      </List>
       <FeedbackCreateDialog open={openDialog} handleClose={handleCloseDialog} />
     </Fragment>
   );
