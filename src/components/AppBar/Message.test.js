b5d2c9a314ed0ee1e6d317969cfd6e3159d1864e
Pillár <Miroslav.Pillar@senacor.com>
2019-10-10 11:30:51 +0200

    another unit tests

--- src/components/AppBar/Message.test.js
+++ src/components/AppBar/Message.test.js
@@ -4,8 +4,21 @@ import { shallowWithIntl } from 'enzyme-react-intl';
 import Message from './Message';
 
 describe('Message', () => {
+  const wrapper = shallowWithIntl(<Message />);
+
   it('should render correctly', () => {
-    const wrapper = shallowWithIntl(<Message />);
     expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('context of message', () => {
+  const mockErrorContext = {
+    hasErrors: false,
+    messageId: ''
+  };
+  const wrapper = shallowWithIntl(<Message />, { mockErrorContext });
+  it('should have text message when error occures', () => {
+    wrapper.setContext({ hasErrors: true, messageId: 'message.error' });
+    expect(wrapper.text()).toBe('<WithStyles(Message) />');
   });
 });
