efe45a56fd523d28c793f56028e848b730d29597
Pillár <Miroslav.Pillar@senacor.com>
2019-11-15 11:28:15 +0100

    performance updated

--- src/components/scs/ScSheet/ScRow.js
+++ src/components/scs/ScSheet/ScRow.js
@@ -5,12 +5,17 @@ import {
   IconButton,
   TextField,
   Tooltip,
-  withStyles
+  withStyles,
+  Paper
 } from '@material-ui/core';
 import ScRatingPoints from '../ScRatingPoints';
 import RemoveIcon from '@material-ui/icons/IndeterminateCheckBox';
 
 const styles = theme => ({
+  scRowContainer: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit
+  },
   removeIcon: {
     position: 'absolute',
     right: 40
@@ -24,139 +29,141 @@ const styles = theme => ({
   }
 });
 
-const ScRow = ({ intl, classes, fields, handleChange, removeFields, type }) => {
-  return (
-    <Fragment>
-      {fields.map((field, index) => {
-        return (
-          <div key={index}>
-            {(type === 'project' || type === 'dailyBusiness') && (
-              <Tooltip
-                title={intl.formatMessage({
-                  id: 'scsheet.tooltip.removeField'
-                })}
-              >
-                <IconButton
-                  className={classes.removeIcon}
-                  onClick={() => removeFields(type, index)}
+const ScRow = React.memo(
+  ({ intl, classes, fields, action, removeSubcategory, type }) => {
+    return (
+      <Fragment>
+        {fields.map((field, index) => {
+          return (
+            <Paper key={index} className={classes.scRowContainer}>
+              {(type === 'project' || type === 'dailyBusiness') && (
+                <Tooltip
+                  title={intl.formatMessage({
+                    id: 'scsheet.tooltip.removeField'
+                  })}
                 >
-                  <RemoveIcon />
-                </IconButton>
-              </Tooltip>
-            )}
-            <div className={classes.textsContainer}>
-              <Grid container spacing={8}>
-                <Grid item sm={6}>
-                  <TextField
-                    type="text"
-                    value={field.title}
-                    margin="normal"
-                    variant="outlined"
-                    label={intl.formatMessage({
-                      id:
-                        type === 'dailyBusiness'
-                          ? 'scsheet.textheader.title.dailyBusiness'
-                          : 'scsheet.textheader.title.project'
-                    })}
-                    fullWidth
-                    className={classes.textarea}
-                    onChange={e => handleChange(type, index, 'title', e)}
-                  />
+                  <IconButton
+                    className={classes.removeIcon}
+                    onClick={() => removeSubcategory(type, index)}
+                  >
+                    <RemoveIcon />
+                  </IconButton>
+                </Tooltip>
+              )}
+              <div className={classes.textsContainer}>
+                <Grid container spacing={8}>
+                  <Grid item sm={6}>
+                    <TextField
+                      type="text"
+                      defaultValue={field.title}
+                      margin="normal"
+                      variant="outlined"
+                      label={intl.formatMessage({
+                        id:
+                          type === 'dailyBusiness'
+                            ? 'scsheet.textheader.title.dailyBusiness'
+                            : 'scsheet.textheader.title.project'
+                      })}
+                      fullWidth
+                      className={classes.textarea}
+                      onChange={e => action(type, index, 'title', e)}
+                    />
+                  </Grid>
+                  <Grid item sm={2}>
+                    <TextField
+                      type="number"
+                      defaultValue={field.weight}
+                      margin="normal"
+                      variant="outlined"
+                      label={intl.formatMessage({
+                        id: 'scsheet.textheader.weight'
+                      })}
+                      fullWidth
+                      className={classes.textarea}
+                      onChange={e => action(type, index, 'weight', e)}
+                    />
+                  </Grid>
+                  <Grid item sm={2}>
+                    <TextField
+                      type="number"
+                      defaultValue={field.percentage}
+                      margin="normal"
+                      variant="outlined"
+                      label={intl.formatMessage({
+                        id: 'scsheet.textheader.percentage'
+                      })}
+                      fullWidth
+                      className={classes.textarea}
+                      onChange={e => action(type, index, 'percentage', e)}
+                    />
+                  </Grid>
+                  <Grid item sm={2} className={classes.textCenter}>
+                    <ScRatingPoints
+                      changeEvaluation={e =>
+                        action(type, index, 'evaluation', e)
+                      }
+                      rating={fields.evaluation}
+                    />
+                  </Grid>
                 </Grid>
-                <Grid item sm={2}>
-                  <TextField
-                    type="number"
-                    value={field.weight}
-                    margin="normal"
-                    variant="outlined"
-                    label={intl.formatMessage({
-                      id: 'scsheet.textheader.weight'
-                    })}
-                    fullWidth
-                    className={classes.textarea}
-                    onChange={e => handleChange(type, index, 'weight', e)}
-                  />
+                <Grid container spacing={8}>
+                  <Grid item sm={4}>
+                    <TextField
+                      type="text"
+                      defaultValue={field.description}
+                      margin="normal"
+                      variant="outlined"
+                      label={intl.formatMessage({
+                        id: 'scsheet.textarea.description'
+                      })}
+                      rows={3}
+                      multiline
+                      fullWidth
+                      className={classes.textarea}
+                      onChange={e => action(type, index, 'description', e)}
+                    />
+                  </Grid>
+                  <Grid item sm={4}>
+                    <TextField
+                      type="text"
+                      defaultValue={field.achievement}
+                      margin="normal"
+                      variant="outlined"
+                      label={intl.formatMessage({
+                        id: 'scsheet.textarea.achievement'
+                      })}
+                      rows={3}
+                      multiline
+                      fullWidth
+                      className={classes.textarea}
+                      onChange={e => action(type, index, 'achievement', e)}
+                    />
+                  </Grid>
+                  <Grid item sm={4}>
+                    <TextField
+                      type="text"
+                      defaultValue={field.comment}
+                      margin="normal"
+                      variant="outlined"
+                      label={intl.formatMessage({
+                        id: 'scsheet.textarea.comment'
+                      })}
+                      rows={3}
+                      multiline
+                      fullWidth
+                      className={classes.textarea}
+                      onChange={e => action(type, index, 'comment', e)}
+                    />
+                  </Grid>
                 </Grid>
-                <Grid item sm={2}>
-                  <TextField
-                    disabled
-                    type="number"
-                    value={field.percentage}
-                    margin="normal"
-                    variant="outlined"
-                    label={intl.formatMessage({
-                      id: 'scsheet.textheader.percentage'
-                    })}
-                    fullWidth
-                    className={classes.textarea}
-                    onChange={e => handleChange(type, index, 'percentage', e)}
-                  />
-                </Grid>
-                <Grid item sm={2} className={classes.textCenter}>
-                  <ScRatingPoints
-                    changeEvaluation={e =>
-                      handleChange(type, index, 'evaluation', e)
-                    }
-                    rating={fields.evaluation}
-                  />
-                </Grid>
-              </Grid>
-              <Grid container spacing={8}>
-                <Grid item sm={4}>
-                  <TextField
-                    type="text"
-                    value={field.description}
-                    margin="normal"
-                    variant="outlined"
-                    label={intl.formatMessage({
-                      id: 'scsheet.textarea.description'
-                    })}
-                    rows={3}
-                    multiline
-                    fullWidth
-                    className={classes.textarea}
-                    onChange={e => handleChange(type, index, 'description', e)}
-                  />
-                </Grid>
-                <Grid item sm={4}>
-                  <TextField
-                    type="text"
-                    value={field.achievement}
-                    margin="normal"
-                    variant="outlined"
-                    label={intl.formatMessage({
-                      id: 'scsheet.textarea.achievement'
-                    })}
-                    rows={3}
-                    multiline
-                    fullWidth
-                    className={classes.textarea}
-                    onChange={e => handleChange(type, index, 'achievement', e)}
-                  />
-                </Grid>
-                <Grid item sm={4}>
-                  <TextField
-                    type="text"
-                    value={field.comment}
-                    margin="normal"
-                    variant="outlined"
-                    label={intl.formatMessage({
-                      id: 'scsheet.textarea.comment'
-                    })}
-                    rows={3}
-                    multiline
-                    fullWidth
-                    className={classes.textarea}
-                    onChange={e => handleChange(type, index, 'comment', e)}
-                  />
-                </Grid>
-              </Grid>
-            </div>
-          </div>
-        );
-      })}
-    </Fragment>
-  );
-};
+              </div>
+            </Paper>
+          );
+        })}
+      </Fragment>
+    );
+  },
+  (prevProps, nextProps) => prevProps.show === nextProps.show
+);
 
 export default injectIntl(withStyles(styles)(ScRow));
