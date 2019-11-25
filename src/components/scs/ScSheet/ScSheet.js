d7a8e06a7df09f4ba9a4774324a0d04ae6bbaf43
mpiroh <matus.piroh@senacor.com>
2019-11-25 13:54:24 +0100

    eslint fix

--- src/components/scs/ScSheet/ScSheet.js
+++ src/components/scs/ScSheet/ScSheet.js
@@ -24,7 +24,6 @@ import {
   reduceWeights,
   updatePercentageAllWithoutPR,
   calculateFinalScoreWithoutPR,
-  calculateFinalScoreWithPR,
   round,
   updatePercentageWithPRPerformance,
   updatePercentageWithPRPrCategories
