9ec012187713a1abba1391af97eaa723fb903e51
smarcin <stefan.marcin@senacor.com>
2019-11-28 08:32:58 +0100

    test 2

--- src/calls/admin.js
+++ src/calls/admin.js
@@ -42,7 +42,7 @@ export const getAllEmployeesWithRoles = async (
   setIsLoading,
   error
 ) => {
-  //TODO: remove this comment
+  //TODO: remove this comment or not?
   try {
     setIsLoading(true);
 
